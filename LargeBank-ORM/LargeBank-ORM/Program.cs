377baeff77e3129161669723ae4673d6949a4b3b
jeffward01 <jeffwardcoding@gmail.com>
2015-10-13 11:26:36 -0700

    version 1.0

--- LargeBank-ORM/LargeBank-ORM/Program.cs
+++ LargeBank-ORM/LargeBank-ORM/Program.cs
@@ -8,8 +8,44 @@ namespace LargeBank_ORM
 {
     class Program
     {
+        //Properties
+        public static string Line = "--------------------------- \n \n \n";
+
         static void Main(string[] args)
         {
+            GreetingsMesage();
+            ListAllCustomersANDAccountInformation();
+         
+
+        }
+        public static void GreetingsMesage()
+        {
+            Console.WriteLine(Line);
+            Console.WriteLine("You have entered the Command Station for Bank Administration");
+            Console.WriteLine("Query Data and More!!");
+            Console.WriteLine(Line);
+            Console.WriteLine("Please press Enter to Continue...");
+            Console.ReadLine();
+            Console.Clear();
+
+        }
+        public static void ListAllCustomersANDAccountInformation()
+        {
+            using (var db = new LargeBankEntities())
+            {
+                foreach (var customer in db.Customers)
+                {
+                    Console.WriteLine("Customer First Name and Last Name: " + customer.FirstName +" "+customer.LastName+".");
+
+                    foreach (var account in customer.Accounts)
+                    {
+                        Console.WriteLine("The balance for account " + account.AccountNumber + " is: " + account.Balance.ToString("C")+ Environment.NewLine);             
+                    }
+                }
+
+                Console.WriteLine("Please press Enter to continue...");
+                Console.ReadLine();
+            }
         }
     }
 }
