4ce8da1edbce0fc0d46d8bdf81502363dbc86b1c
jeffward01 <jeffwardcoding@gmail.com>
2015-10-13 12:12:48 -0700

    added save Customer function

--- LargeBank-ORM/LargeBank-ORM/Program.cs
+++ LargeBank-ORM/LargeBank-ORM/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LargeBank_ORM.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,8 +16,9 @@ namespace LargeBank_ORM
         {
             GreetingsMesage();
             ListAllCustomersANDAccountInformation();
-         
-
+            GetSAVENewCustomerInfo();
+           
+        
         }
         public static void GreetingsMesage()
         {
@@ -47,5 +49,78 @@ namespace LargeBank_ORM
                 Console.ReadLine();
             }
         }
+
+        public static void GetSAVENewCustomerInfo()
+        {
+            Console.WriteLine("You are about to add a new Customer to your Database!");
+            Console.WriteLine(Line);
+            Console.WriteLine("Please enter the New Customers information");
+
+            //Get New Customers First Name
+            Console.WriteLine("Please enter the Customer's First Name");
+            string newCustFirstName = Console.ReadLine();
+
+            //Get New Customers Lasst Name
+            Console.WriteLine("Please Enter the Customer's Last Name");
+            string newCustLastName = Console.ReadLine();
+
+            //Get New Customers Address1
+            Console.WriteLine("Please enter the Customers Address 1");
+            string newCustAddress1 = Console.ReadLine();
+
+            //Get New Customers Address2
+            Console.WriteLine("Please enter the Customers Address 2 (if none, press enter)");
+            string newCustAddress2 = Console.ReadLine();
+
+            if (newCustAddress2 == null)
+            {
+                newCustAddress2 = " ";
+            }
+
+            //Get New Customers City
+            Console.WriteLine("Please enter the Customers City");
+            string newCustCity = Console.ReadLine();
+
+            //Get New Customers State
+            string newCustState = JeffToolBox.getClientState("Please enter the Customers State in 2 Characters ie: (CA   TX   MD)", true);
+
+            //Get Zip Code
+            decimal ZipCode = JeffToolBox.ReadDecimalZipCode("Please enter a 5-Digit Zip Code", true, false);
+            string newCustZipCode = ZipCode.ToString();
+
+            //Save new Customer in DB
+            SaveNewCustomerinDB(newCustFirstName, newCustLastName, newCustAddress1, newCustAddress2, newCustCity, newCustState, newCustZipCode);
+        }
+        public static void SaveNewCustomerinDB(string firstName, string lastName, string address1, string address2, string city, string state, string zip)
+        {
+            //Access my Database
+            using (var db = new LargeBankEntities())
+            {
+                try
+                {
+                    //Build Instance of Student Class/Table
+                    Customer customer = new Customer();
+                    customer.FirstName = firstName;
+                    customer.LastName = lastName ;
+                    customer.Address1 = address1;
+                    customer.Address2 = address2;
+                    customer.CreatedDate = DateTime.Now;
+                    customer.City = city;
+                    customer.States = state;
+                    customer.Zip = zip;
+
+                    //Add new Instance of Customer into DateBase
+                    db.Customers.Add(customer);
+                    db.SaveChanges();
+                    Console.WriteLine("Your New Customer has been added to the Database. \n Created on: {6} \n \n  Your Customers name is: {0} {1}. \n Address: {2} \n {7} \n City: {3} \n State: {4} \n Zip: {5}.", customer.FirstName, customer.LastName, customer.Address1, customer.City, customer.States, customer.Zip, customer.CreatedDate, customer.Address2);
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine("Your new Customer could not be saved. Please try again or contact System Admin.");
+                }
+            }
+
+        }
+
     }
 }
